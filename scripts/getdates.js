b36c5cf8430b7e5fc2370e77202b04a2c47f4bb4
Gunner White <gunnerwhite00@gmail.com>
2024-11-09 02:55:54 -0700

    Wk1 Practice

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").textContent = `© ${currentYear}`;
+
+document.getElementById("lastModified").textContent = document.lastModified;
